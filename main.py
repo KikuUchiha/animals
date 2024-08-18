a0c9f2878021499f90dd099544f8f55dd03782b7
Kiku <n.u.10.10@mail.ru>
2024-08-18 14:03:50 +0300

    Добавлен класс PackAnimal

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ class Pet(Animal):
     def __init__(self, name, birthdate, commands):
         super().__init__(name, birthdate)
         self.commands = commands
+        
+class PackAnimal(Animal):
+    def __init__(self, name, birthdate, load_capacity):
+        super().__init__(name, birthdate)
+        self.load_capacity = load_capacity
